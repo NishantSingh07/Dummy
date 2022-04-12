5c2a5729d3643e484d9d1bd665bcae99275fb39f
Singh <nishant.b.singh@capgemini.com>
2022-04-12 17:16:29 +0530

    Initial commit

---
+++ src/main/java/Dummy.java
@@ -0,0 +1,6 @@
+public class Dummy {
+    public static void main(String[] args) {
+        System.out.println("First Name is "+ System.getenv("FirstName"));
+        System.out.println("Last Name is "+ System.getenv("LastName"));
+    }
+}
